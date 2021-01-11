6a830038e06b9d92d79087915401ca3fdbf097b8
Pavel Semkin <pvsemk@mail.ru>
2021-01-11 14:08:55 +0300

    Added argument to pose estimation vis

--- modelplace_api/visualization.py
+++ modelplace_api/visualization.py
@@ -266,6 +266,7 @@ def draw_pose_estimation_result(
     image: Union[Image, np.ndarray],
     detections: List[Pose],
     confidence_threshold: float,
+    simple_labels: bool = False,
 ) -> List[np.ndarray]:
     image_with_skeletons = np.ascontiguousarray(image)
     source_img = image_with_skeletons.copy()
@@ -309,21 +310,39 @@ def draw_pose_estimation_result(
             for i, joint in enumerate(unique_joints):
                 if joint.x == joint.y == 0:
                     continue
-                cv2.circle(
-                    image_with_skeletons,
-                    (int(joint.x), int(joint.y)),
-                    4,
-                    RGB_COLORS[i][::-1],
-                    -1,
-                )
-                cv2.circle(
-                    one_pose_image,
-                    (int(joint.x), int(joint.y)),
-                    4,
-                    RGB_COLORS[i][::-1],
-                    -1,
-                )
+                if not simple_labels:
+                    cv2.circle(
+                        image_with_skeletons,
+                        (int(joint.x), int(joint.y)),
+                        4,
+                        RGB_COLORS[i][::-1],
+                        -1,
+                    )
+                    cv2.circle(
+                        one_pose_image,
+                        (int(joint.x), int(joint.y)),
+                        4,
+                        RGB_COLORS[i][::-1],
+                        -1,
+                    )
+                else:
+                    cv2.circle(
+                        image_with_skeletons,
+                        (int(joint.x), int(joint.y)),
+                        4,
+                        RGB_COLORS[len(unique_joints)][::-1],
+                        -1,
+                    )
+                    cv2.circle(
+                        one_pose_image,
+                        (int(joint.x), int(joint.y)),
+                        4,
+                        RGB_COLORS[len(unique_joints)][::-1],
+                        -1,
+                    )
             images.append(one_pose_image)
+            if simple_labels:
+                class_map = {"keypoint": RGB_COLORS[len(unique_joints)][::-1]}
 
     images.append(image_with_skeletons)
     return [draw_legend(image, class_map) for image in images]
