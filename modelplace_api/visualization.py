c612b985cc48448a6c753f064d6f519650e46479
Alexander Slavutin <alexander.slavutin@xperience.ai>
2021-01-13 14:03:16 +0300

    change wrong return type

--- modelplace_api/visualization.py
+++ modelplace_api/visualization.py
@@ -520,9 +520,10 @@ def draw_classification_result(
 
 def draw_age_gender_recognition_result(
     image: Union[Image, np.ndarray], detections: List[AgeGenderLabel],
-) -> np.ndarray:
+) -> List[np.ndarray]:
     image_with_boxes = np.ascontiguousarray(image)
     source_image = image_with_boxes.copy()
+    images = list()
     images = list()
     images.append(source_image)
     for detection in detections:
@@ -546,7 +547,7 @@ def draw_age_gender_recognition_result(
 
 def draw_emotion_recognition_result(
     image: Union[Image, np.ndarray], detections: List[AgeGenderLabel],
-) -> np.ndarray:
+) -> List[np.ndarray]:
     image_with_boxes = np.ascontiguousarray(image)
     source_image = image_with_boxes.copy()
     images = list()
